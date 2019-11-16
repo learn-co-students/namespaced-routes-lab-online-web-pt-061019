a7e75075dc2d254e31c35ab09c7422b747bb4411
Khris Punzalan <khrispunzalan@gmail.com>
2019-11-16 12:56:37 -0800

    Done.

--- db/migrate/20191116205434_create_preferences.rb
+++ db/migrate/20191116205434_create_preferences.rb
@@ -1,8 +1,6 @@
 class CreatePreferences < ActiveRecord::Migration[5.0]
   def change
     create_table :preferences do |t|
-      t.string :song_sort_order
-      t.string :artist_sort_order
       t.boolean :allow_create_artists
       t.boolean :allow_create_songs
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191116202329) do
+ActiveRecord::Schema.define(version: 20191116205434) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -19,8 +19,6 @@ ActiveRecord::Schema.define(version: 20191116202329) do
   end
 
   create_table "preferences", force: :cascade do |t|
-    t.string   "song_sort_order"
-    t.string   "artist_sort_order"
     t.boolean  "allow_create_artists"
     t.boolean  "allow_create_songs"
     t.datetime "created_at",           null: false
